58957e3bf5cee5fa7b1bbb41bd33289da34b7987
Matthieu VETOIS <matthieu.vetois@epitech.eu>
2020-07-11 02:04:42 +0200

    Add : Adding clear function (this function delete the requested number of messages)

--- main.ts
+++ main.ts
@@ -3,18 +3,15 @@ const client = new Discord.Client();
 const token = require("./token.json");
 const config = require("./config.json");
 
-client.token = token;
-client.config = config;
-
 client.on("ready", function () {
     console.log("Bot started !");
 })
 
 client.on("message", (msg) => {
     var args = msg.content.toLowerCase().split(" ");
-    var admin = msg.member.roles.cache.some(role => role.name === client.config.roles.admin);
+    var admin = msg.member.roles.cache.some(role => role.name === config.roles.admin);
 
-    if (args[0] === (client.config.prefix + "ping")) {
+    if (args[0] === (config.prefix + "ping")) {
         if (admin) {
             msg.react("👌");
             msg.reply("Pong !");
@@ -22,7 +19,29 @@ client.on("message", (msg) => {
             msg.channel.send("Pong !")
         }
         return;
+    } else if (args[0] === (config.prefix + "clear")) {
+        if (admin) {
+            if (!args[1]) {
+                msg.reply(config.messages.clear.noArgs);
+                return;
+            } else if (isNaN(args[1])) {
+                msg.reply(config.messages.clear.noDigit);
+                return;
+            } else if (args[1] > config.config.clear.maxClear) {
+                msg.reply(config.messages.clear.maxClear + config.config.clear.maxClear);
+                return;
+            } else if (args[1] < config.config.clear.minClear) {
+                msg.reply(config.messages.clear.minClear + config.config.clear.minClear);
+                return;
+            }
+            msg.channel.messages.fetch({ limit: args[1] }).then(messages => {
+                msg.channel.bulkDelete(messages
+            )});
+        } else {
+            msg.delete();
+        }
+        return;
     }
 })
 
-client.login(client.token.token);
+client.login(token.token);
